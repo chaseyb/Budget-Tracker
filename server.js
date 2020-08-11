50c6d287a2ada3d1a22e8349cee23ea622242165
Chase Johnson <Chase.b.johnson@gmail.com>
2020-08-11 11:07:25 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+// Express 
 const app = express();
-
-app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Logger 
+app.use(logger("dev"));
+
+// Mongoose Connection to DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password2020@ds041357.mlab.com:41357/heroku_mv53g3jb", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -25,6 +25,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password2020@ds04135
 // routes
 app.use(require("./routes/api.js"));
 
+// Server 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
